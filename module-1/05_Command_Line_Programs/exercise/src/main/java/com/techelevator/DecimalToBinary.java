349b3c8279d8e495233c8ed5a87da551c1f41e49
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-05-21 08:48:22 -0400

    actual final Command promp programs

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DecimalToBinary {
 	public static void main(String[] args) {
 		Scanner decimalScanner = new Scanner(System.in);
-		System.out.println("type");
+		System.out.println("Please enter in a series of decimal values (separated by spaces)");
 		String decimalString = decimalScanner.nextLine();
 		String[] stringParts =decimalString.split(" ");
 
