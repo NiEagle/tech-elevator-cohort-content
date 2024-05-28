3159d53ec0fef2f1cf80e7badfbea491950f5e9c
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-05-27 21:19:12 -0400

    HomeWorkAssignment class made

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,10 +2,12 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
-//CLASS VARIABLES
+
+
+    //CLASS VARIABLES
    private int earnedMarks;
    private int possibleMarks;
-   private int submitterName;
+   private String submitterName;
 
 //GETTERS
     public int getEarnedMarks() {
@@ -14,11 +16,33 @@ public class HomeworkAssignment {
     public int getPossibleMarks() {
         return possibleMarks;
     }
-    public int getSubmitterName() {
+    public String getSubmitterName() {
         return submitterName;
-
+    }
 //SETTERS
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+//Constructor
+     public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+    //Methods
+    public String getLetterGrade() {
+        double gradePercent = (double) this.earnedMarks / (double) this.possibleMarks;
 
+        if (gradePercent * 100 >= 90) {
+            return "A";
+        } else if (gradePercent * 100 < 90 && gradePercent * 100 >= 80) {
+            return "B";
+        } else if (gradePercent * 100 < 80  && gradePercent * 100 >= 70) {
+            return "C";
+        } else if (gradePercent * 100 < 70 && gradePercent * 100 >= 60) {
+            return "D";
+        }
+        return "F";
 
     }
+
 }
