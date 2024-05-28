eee32035bbc4aea23781bfec05527ee58ff4e44e
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-05-27 20:49:21 -0400

    finshed television class

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -2,9 +2,9 @@ package com.techelevator;
 
 public class Television {
 //Class Variables
-    private boolean isOn;
-    private int currentChannel;
-    private int currentVolume;
+    private boolean isOn = false;
+    private int currentChannel = 3;
+    private int currentVolume = 2;
 
 
 //Getters
@@ -18,7 +18,63 @@ public class Television {
     public int getCurrentVolume() {
         return currentVolume;
     }
+//methods
+    public void turnOff(){
+        if(this.isOn){
+            this.isOn = false;
+        }
+    }
+    public void turnOn(){
+        if(!this.isOn){
+            this.isOn = true;
+        }
+    }
+      public void changeChannel(int newChannel){
+        if(this.isOn){
+            if(newChannel > this.currentChannel){
+            this.currentChannel = this.currentChannel + (newChannel - this.currentChannel);
+            }
+            else if(newChannel < this.currentChannel){
+                this.currentChannel = this.currentChannel + (newChannel - this.currentChannel);
+            }
+            }
+      }
+    public void channelUp(){
+        if(this.isOn) {
+            if (this.currentChannel <= 17) {
+                this.currentChannel = this.currentChannel + 1;
+            }
+            else if(this.currentChannel == 18) {
+                this.currentChannel = this.currentChannel - 15;
+            }
+        }
+    }
 
+    public void channelDown(){
+            if(this.isOn){
+                if (this.currentChannel >= 4) {
+                this.currentChannel = this.currentChannel - 1;
+            }
+                else if(this.currentChannel == 3) {
+                this.currentChannel = this.currentChannel + 15;
+                }
+            }
+    }
+   public void raiseVolume(){
+        if(this.isOn){
+            if (this.currentVolume <= 9) {
+           this.currentVolume = this.currentVolume + 1;
+            }
+        }
+
+   }
+  public void lowerVolume(){
+      if(this.isOn){
+          if (this.currentVolume >= 1) {
+              this.currentVolume = this.currentVolume - 1;
+          }
+      }
+  }
 
 
 
