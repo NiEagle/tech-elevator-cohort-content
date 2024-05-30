4c4ba65dce23e8b64365299fa3d83e66ac97f2ed
Steve Carmichael <steve@techelevator.com>
2024-05-30 13:21:08 -0400

    added final lecture code

---
+++ module-1/13_Managing_Inheritance/lecture-final/src/main/java/com/techelevator/bike/Bike.java
@@ -0,0 +1,12 @@
+package com.techelevator.bike;
+
+public abstract class Bike {
+
+    private int gears;
+
+    public abstract void ride();
+
+    public int getGears() {
+        return gears;
+    }
+}
---
+++ module-1/13_Managing_Inheritance/lecture-final/src/main/java/com/techelevator/bike/MountainBike.java
@@ -0,0 +1,8 @@
+package com.techelevator.bike;
+
+public  class MountainBike extends Bike{
+
+    public void  ride() {
+        System.out.println("we have " + getGears());
+    }
+}
