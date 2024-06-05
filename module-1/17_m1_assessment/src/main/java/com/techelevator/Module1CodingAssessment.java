d1d9ab4a60c4728427aa9c81350fce9099ef01a2
Steve Carmichael <steve@techelevator.com>
2024-06-05 09:12:08 -0400

    added assessment

---
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+public class Module1CodingAssessment {
+
+	public static void main(String[] args) {
+		Module1CodingAssessment mca = new Module1CodingAssessment();
+		mca.run();
+	}
+
+	private void run() {
+
+           //start coding here... You will call other class or classes from here as needed, or write additional methods. 
+
+
+	}
+
+}
