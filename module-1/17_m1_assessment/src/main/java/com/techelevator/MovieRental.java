4d41e99757c1cf0885c5597791fafeb0acd277b7
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-05 09:22:48 -0400

    step 1 and 2 complete

---
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/MovieRental.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class MovieRental {
+    //CLASS VARIABLES
+    /*
+    title: indicates the title of the movie.
+format: indicates the format of the movie (VHS, DVD, or Blu-ray).
+is premium movie: indicates if the movie is a premium movie. Premium movies cost more.
+rental price: indicates the rental price (VHS $0.99, DVD $1.99, BluRay $2.99). Premium movies add an additional $1.00 to the rental price. (Hint: What could you call from outside of the object to return the appropriate rental price? How could you derive the appropriate price?)
+     */
+    private String titleOfMovie;
+    private String formatOfMovie;
+    private boolean isMoviePremium;
+    private double priceOfMovie;
+
+}
