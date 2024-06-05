4ad0a939df071dc81ab6e0f9ad0a74bc6390e0c3
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-05 10:16:33 -0400

    step 7 complete

--- module-1/17_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/17_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -8,25 +8,35 @@ public class Module1CodingAssessment {
 	public static void main(String[] args) {
 		Module1CodingAssessment mca = new Module1CodingAssessment();
 		mca.run();
+
+
 		List<MovieRental> moviesToRent = new ArrayList<>();
 
-		MovieRental theThing = new MovieRental("The_THING","VHS",true);
-		MovieRental theOtherThing = new MovieRental("The_OTHER_THING","Blu-ray",false);
-		MovieRental theReturnOfTheThing = new MovieRental("The_Return_Of_The_THING","DVD",false);
+		MovieRental theThing = new MovieRental("The_THING", "VHS", true);
+		MovieRental theOtherThing = new MovieRental("The_OTHER_THING", "Blu-ray", false);
+		MovieRental theReturnOfTheThing = new MovieRental("The_Return_Of_The_THING", "DVD", false);
 
 		moviesToRent.add(theThing);
 		moviesToRent.add(theOtherThing);
 		moviesToRent.add(theReturnOfTheThing);
 
 
+		/*
+		Back in the Module1CodingAssessment class, create a private method called printMovieList that takes an ArrayList from the previous step and prints out each movie and it's data. (Hint: can you call toString()). Note: You should be 'passing' the ArrayList into this private method from the run() method. Do not write this code in the run() method.
+		 */
+		private void printMovieList(moviesToRent) {
+			for (MovieRental m : moviesToRent) {
+				System.out.println(m.toString());
+			}
+		}
 
-	}
 
-	private void run() {
+		private void run () {
 
-           //start coding here... You will call other class or classes from here as needed, or write additional methods. 
+			//start coding here... You will call other class or classes from here as needed, or write additional methods.
 
 
+		}
 	}
 
 }
