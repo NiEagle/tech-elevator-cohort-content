fa568048a6c81d00ae19444b9d10447c5022afe4
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-12 09:04:39 -0400

    daily pull

--- module-2/02_Subqueries_and_Aggregation/lecture/lecture.sql
+++ module-2/02_Subqueries_and_Aggregation/lecture/lecture.sql
@@ -1,9 +1,13 @@
 -- CONCATENATING OUTPUTS
+SELECT FROM city;
+SELECT FROM park
 
 -- All city names and their state abbreviation.
-
+SELECT cit_name, state_abbreviation FROM city
 
 -- The all parks by name and date established.
+SELECT (park_name ||'-'|| date_established) FROM park;
+
 
 
 -- The census region and state name of all states in the West & Midwest sorted in ascending order.
@@ -13,18 +17,31 @@
 -- SUBQUERIES
 
 -- List all cities in the western census region
-
+SELECT city_name,state_abbreviation
+FROM city
+WHERE state_abbreviation IN(SELECT state_abbreviation FROM state WHERE census_region = 'West')
+;
 
 -- List all cities in states with "garden" in their nickname
 
-
+SELECT *
+FROM city
+WHERE state_abbreviation IN(SELECT state_abbreviation FROM state WHERE state_nickname ILIKE '%garden%') 
 -- Get the parks with the smallest and largest areas
-
+SELECT park_name, area
+FROM park
+WHERE area = (SELECT MIN(area) FROM park)
+OR area = (SELECT MAX(area) FROM park)
 
 -- Get the state name and census region for all states with a national park
-
+SELECT state_name, census_Region 
+FROM state
+WHERE state_abbreviation IN (SELECT state_abbreviation FROM park_state)
 
 -- Subqueries aren't limited to WHERE clauses, can appear in SELECT list (must return only 1 result)
+SELECT count(population) 
+FROM city
+WHERE POPULATION > 100000000
 -- Include state name rather than the state abbreviation while counting the number of cities in each state,
 --   ordered from most cities to least.
 
@@ -51,12 +68,20 @@
 -- GROUP BY
 
 -- Count the number of cities in each state, ordered from most cities to least.
+SELECT count(city_name) AS city_count, state_abbreviation FROM city
+GROUP BY state_abbreviation
+ORDER BY city_count DESC
 
 
 -- Determine the average park area depending upon whether parks allow camping or not.
-
+SELECT has_camping, AVG(area)
+FROM park
+GROUP BY has_camping
 
 -- Sum of the population of cities in each state ordered by state abbreviation.
+SELECT state_abbreviation, SUM(population) FROM city
+GROUP BY state_abbreviation
+ORDER BY state_abbreviation
 
 
 -- The smallest city population in each state ordered by city population.
