06e087ea70d8a6ffec73da53cb0603d05554ef56
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-13 15:34:28 -0400

    thru join HW

--- module-2/03_Joins/exercise/Exercises/12_star_wars_collection_not_lucas.sql
+++ module-2/03_Joins/exercise/Exercises/12_star_wars_collection_not_lucas.sql
@@ -1,3 +1,9 @@
 -- 12. The titles of the movies in the "Star Wars Collection" that weren't directed by George Lucas, sorted alphabetically.
 -- (5 rows)
 
+SELECT title
+FROM movie
+JOIN collection ON movie.collection_id = collection.collection_id
+JOIN person ON director_id = person_id
+WHERE person_name != 'George Lucas' AND collection_name = 'Star Wars Collection'
+ORDER BY title
--- module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -2,3 +2,9 @@
 -- Order the results by movie title (A-Z)
 -- (73 rows)
 
+SELECT title, person_name
+FROM person
+JOIN movie_actor ON person.person_id = movie_actor.actor_id
+JOIN movie ON movie_actor.movie_id = movie.movie_id
+WHERE person_id = director_id
+ORDER BY title
