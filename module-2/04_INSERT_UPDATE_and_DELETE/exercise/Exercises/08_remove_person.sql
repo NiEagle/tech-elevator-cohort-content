b1eed8d97476a3c3f56a8686f4e6d795a1b37d9c
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-17 08:42:14 -0400

    stuff2

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/08_remove_person.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/08_remove_person.sql
@@ -2,7 +2,7 @@
 -- You'll have to remove data from another table before you can make him "disappear" (Get it? Because he's a magician...) (1 row each)
 
 
-DELETE FROM movie_actor WHERE actor_id = (SELECT person_id FROM person WHERE person_name = 'Penn Jillett');
-DELETE FROM person WHERE person_name = 'Penn Jillett';
+DELETE FROM movie_actor WHERE actor_id = (SELECT person_id FROM person WHERE person_name = 'Penn Jillette');
+DELETE FROM person WHERE person_name = 'Penn Jillette';
 
 
