f103a21d1a9e9876f1652907a41c8bc213b65981
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-06-19 20:26:41 -0400

    Half data-access1

--- module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ module-2/06_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -2,9 +2,11 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Collection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class JdbcCollectionDao implements CollectionDao{
@@ -17,16 +19,54 @@ public class JdbcCollectionDao implements CollectionDao{
 
     @Override
     public List<Collection> getCollections() {
-        return null;
+        List<Collection> Collections = new ArrayList<>();
+        String sql = "SELECT * " +
+                "FROM collection"
+                ;
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            Collections.add(mapRowToCollection(results));
+        }
+        return Collections;
     }
-
     @Override
     public Collection getCollectionById(int id) {
-        return new Collection(-1, "Not implemented yet");
+        Collection collection = null;
+        String sql = "SELECT * " +
+                "FROM collection " +
+                "WHERE collection_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,id);
+        if (results.next()) {
+            collection = mapRowToCollection(results);
+        }
+        return collection;
     }
 
     @Override
     public List<Collection> getCollectionsByName(String name, boolean useWildCard) {
-        return null;
+        List<Collection> Collections = new ArrayList<>();
+
+        String sql = "SELECT * " +
+                "FROM collection " +
+                "WHERE collection_name ILIKE ?";
+
+        if (useWildCard)
+        {
+            name = "%" + name + "%";
+        }
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, name);
+        while (results.next()) {
+            Collections.add(mapRowToCollection(results));
+        }
+
+        return Collections;
+    }
+    private Collection mapRowToCollection(SqlRowSet rowSet) {
+        Collection collection = new Collection();
+        collection.setId(rowSet.getInt("collection_id"));
+        collection.setName(rowSet.getString("collection_name"));
+
+        return collection;
     }
 }
