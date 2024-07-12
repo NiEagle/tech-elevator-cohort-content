b2d8e1f8dab13be7fb45e369c879e7c3096749b3
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-07-12 13:38:52 -0400

    worked on grid HW

---
+++ module-3/06_Intro_to_JavaScript/lecture/arrays.js
@@ -0,0 +1,15 @@
+console.log("hi");
+
+const names = ['Alex','Conner','Madeline','Nick'];
+console.log(names);
+
+let x = names.length;
+console.log("The length is:" + x);
+
+names.push('Gabby');
+console.log(names);
+console.log(names[2]);
+console.log("hi","there");
+
+let z= ("hi","there");
+console.log(z);
