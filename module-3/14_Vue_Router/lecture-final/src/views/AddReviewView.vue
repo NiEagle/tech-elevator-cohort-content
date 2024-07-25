e1b3a56279db180bbf56227abada92f952758cae
Steve Carmichael <steve@techelevator.com>
2024-07-25 13:18:21 -0400

    added final lecture code

---
+++ module-3/14_Vue_Router/lecture-final/src/views/AddReviewView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Add a review:</h1>
+    <add-review v-bind:productId="getProductId"/>
+  </div>
+</template>
+
+<script>
+
+import AddReview from '../components/AddReview.vue';
+
+export default {
+    components: {
+        AddReview
+    },
+    computed: {
+        getProductId() {
+            return this.$route.params.id
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/14_Vue_Router/lecture-final/src/views/HappyHappyJoyJoyView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Happy Happy Joy Joy!</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
