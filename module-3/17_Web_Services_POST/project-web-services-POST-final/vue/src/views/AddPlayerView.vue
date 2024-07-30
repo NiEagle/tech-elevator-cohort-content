01fba99cf9689475ae91cbef5a590fcfd857571e
Steve Carmichael <steve@techelevator.com>
2024-07-30 12:14:22 -0400

    added more

---
+++ module-3/17_Web_Services_POST/project-web-services-POST-final/vue/src/views/AddPlayerView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Add Player</h1>
+  <add-player />
+</template>
+
+<script>
+
+import AddPlayer from '../components/AddPlayer.vue';
+
+
+export default {
+
+    components: {
+        AddPlayer
+    }
+
+}
+</script>
+
+<style>
+
+</style>
