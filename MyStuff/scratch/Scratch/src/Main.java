ff0c72166fef64b84783ac55bf85fc2f49eb3b49
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-08-15 12:28:47 -0400

    f

---
+++ MyStuff/scratch/Scratch/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\n%-5s %-5s %-5s","fill","drill","bill");
+        System.out.printf("\n%-5s %-5s %-5s","ma","pa","lol");
+        System.out.printf("\n%-5s %-5s %-5s","a","b","b");
+
+    }
+}
